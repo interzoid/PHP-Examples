750a94722fa8ce6300f747a9f0fa031df8cb1156
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-09-06 00:17:46 -0700

    Create GetGlobalNumberInformation.php

---
+++ GetGlobalNumberInformation.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+// Create an http client
+$client = new \GuzzleHttp\Client();
+
+// Call the API and store response
+$response = $client->request('GET', 'https://api.interzoid.com/getglobalnumberinfo?license=YOURAPIKEY&intlnumber=+4906979550');
+
+// show response status code
+echo $response->getStatusCode();
+
+// show response data
+echo $response->getBody();
