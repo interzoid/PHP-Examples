019719bf5a60e3b7408bcd91ae451ec7c5b8742b
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-09-06 00:14:12 -0700

    Create GetEmailInformation.php

---
+++ GetEmailInformation.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+// Create an http client
+$client = new \GuzzleHttp\Client();
+
+// Call the API and store response
+$response = $client->request('GET', 'https://api.interzoid.com/getemailinfo?license=YOURAPIKEY&email=winston.smith@maths.ox.ac.uk');
+
+// show response status code
+echo $response->getStatusCode();
+
+// show response data
+echo $response->getBody();
