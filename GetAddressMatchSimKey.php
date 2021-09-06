9855c4b940c6417eb925bb88e847fd9af857bb0c
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-09-05 19:11:20 -0700

    Create GetAddressMatchSimKey.php

---
+++ GetAddressMatchSimKey.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+// Create an http client
+$client = new \GuzzleHttp\Client();
+
+// Call the API and store response
+$response = $client->request('GET', 'https://api.interzoid.com/getaddressmatchadvanced?license=YOURAPIKEY&address=500 main street suite 101&algorithm=narrow');
+
+// show response status code
+echo $response->getStatusCode();
+
+// show response data
+echo $response->getBody();
