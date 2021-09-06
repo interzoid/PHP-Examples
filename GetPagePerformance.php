a7f477a097f15d528d02a9c4b7249e505abac65f
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-09-06 06:35:14 -0700

    Create GetPagePerformance.php

---
+++ GetPagePerformance.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+// Create an http client
+$client = new \GuzzleHttp\Client();
+
+// Call the API and store response
+$response = $client->request('GET', 'globalpageload?license=YOURAPIKEY&origin=Singapore&url=http://www.yahoo.com');
+
+// show response status code
+echo $response->getStatusCode();
+
+// show response data
+echo $response->getBody();
